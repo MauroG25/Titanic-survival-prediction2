22ca97715c8b16ab06814c15cb97a6887277a3e4
MauroG25 <161057993+MauroG25@users.noreply.github.com>
2026-05-11 22:55:38 -0400

    data cleaning
    how to clean the data

---
+++ 02_data_cleaning.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+
+df = pd.read_csv("titanic.csv")
+print("Initial Data Shape:", df.shape)
+print("First 5 rows of the dataset:")
+print(df.head())
+
+print(df.isnull().sum())
+print(df.isnull().sum().sum())
+
+df["HasCabin"] = df["Cabin"].notnull().astype(int)
+
+print(df.groupby("HasCabin")["Survived"].mean())
+
+df = df.drop(columns = ["Cabin"])
+
+
+print(df.groupby(["Pclass", "Sex"])["Age"].median())
+
+print(df["Age"].isnull().sum())
+
+df["Age"] = df.groupby(["Pclass", "Sex"])["Age"].transform(lambda x: x.fillna(x.median()))
+
+
+df["Age"] = df["Age"].fillna(df["Age"].median())
+
+print(df["Age"].isnull().sum())
+
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.hist(df["Age"], bins=30, color="#3498db", edgecolor="black")
+ax.set_title("Age Distribution After Imputation")
+ax.set_xlabel("Age")
+ax.set_ylabel("count")
+plt.tight_layout()
+plt.show()
+
+print(df["Embarked"].isnull().sum())
+
+print(df["Embarked"].mode()[0])
+
+df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
+
+# let see the null values again
+
+print(df.isnull().sum().sum())
+
+print(df.isnull().sum())
+
+df["Title"] = df["Name"].apply(lambda x: x.split(",")[1].split(".")[0].strip())
+print(df["Title"].value_counts())
+
+title_mapping = {
+    "Mr": "Mr",
+    "Miss": "Miss",
+    "Mrs": "Mrs",
+    "Master": "Master",
+}
+
+df["Title"] = df["Title"].apply(lambda x: title_mapping.get(x, "Rare"))
+
+print(df["Title"].value_counts())
+
+print(df.groupby("Title")["Survived"].mean().sort_values(ascending=False))
+
